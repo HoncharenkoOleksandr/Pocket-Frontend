7172652bf5a3ff3651242242fc70e3b6d6519adc
Oleksandr Honcharenko <oleksandr.honcharenko@litslink.com>
2022-08-14 19:19:18 +0300

    fix: 🔧 configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,22 @@
 module.exports = {
   content:["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0A5DFF",
+        success: "#20C933",
+        warning: "#FFA800",
+        error: "#FF6E6E",
+        white: "#FFF",
+        gray: {
+          light: "#F7F7F7",
+          DEFAULT: "#EBEBEB",
+          dark: "#B2B2B2"
+        },
+        black: "#000000",
+        purple: "#624CE0"
+      },
+    },
   },
   plugins: [],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import viteSvgr from 'vite-plugin-svgr';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [viteSvgr(), react()],
+  resolve: {
+    alias: {
+      '@common': path.resolve(__dirname, './src/common'),
+      '@common/*': path.resolve(__dirname, './src/common'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@utils/api': path.resolve(__dirname, './src/utils/api'),
+      '@utils/constants': path.resolve(__dirname, './src/utils/constants')
+    }
+  }
+});
