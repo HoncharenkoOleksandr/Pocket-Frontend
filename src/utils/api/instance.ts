ef5c183d3102d0591ba066e9a40f33bda06b3f70
Oleksandr Honcharenko <oleksandr.honcharenko@litslink.com>
2022-09-20 19:04:25 +0300

    feat: url to deployed backend

--- src/utils/api/instance.ts
+++ src/utils/api/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://pocket--backend.herokuapp.com',
 });
 
 api.interceptors.request.use<AxiosRequestConfig>((config: AxiosRequestConfig) => {
