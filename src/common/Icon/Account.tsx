98ffc6e0db7f92d850f608158e807b775c6c7f9c
Oleksandr Honcharenko <oleksandr.honcharenko@litslink.com>
2022-09-22 02:58:38 +0300

    fix: conflicts & eof problem

--- src/common/Icon/Account.tsx
+++ src/common/Icon/Account.tsx
@@ -1,29 +1,1 @@
-import * as React from "react"
-import { FC } from 'react';
-
-interface IconProps {
-  color?: string;
-  opacity?: number;
-}
-
-export const IconAccount: FC<IconProps> = ({color = "#fff", opacity = 0.4}) => (
-  <svg
-    width={20}
-    height={20}
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M8.086 10.453a5.001 5.001 0 0 0 5.45-8.155 5 5 0 1 0-5.45 8.155Z"
-      fill={color}
-    />
-    <path
-      opacity={opacity}
-      d="M16.666 18.333H3.333v-1.666A4.167 4.167 0 0 1 7.5 12.5h5a4.167 4.167 0 0 1 4.166 4.167v1.666Z"
-      fill={color}
-    />
-  </svg>
-)
-
+import * as React from 'react';import { FC } from 'react';interface IconProps {  color?: string;  opacity?: number;}export const IconAccount: FC<IconProps> = ({ color = '#fff', opacity = 0.4 }) => (  <svg width={20} height={20} fill='none' xmlns='http://www.w3.org/2000/svg'>    <path      fillRule='evenodd'      clipRule='evenodd'      d='M8.086 10.453a5.001 5.001 0 0 0 5.45-8.155 5 5 0 1 0-5.45 8.155Z'      fill={color}    />    <path      opacity={opacity}      d='M16.666 18.333H3.333v-1.666A4.167 4.167 0 0 1 7.5 12.5h5a4.167 4.167 0 0 1 4.166 4.167v1.666Z'      fill={color}    />  </svg>);
--- src/common/Icon/Logout.tsx
+++ src/common/Icon/Logout.tsx
@@ -7,20 +7,16 @@ interface IconProps {
 }
 export const IconLogout: FC<IconProps> = ({ color = '#fff', ...props }) => (
   <svg width={20} height={20} xmlns='http://www.w3.org/2000/svg' fill='none' {...props}>
-    {' '}
     <g clipPath='url(#a)'>
-      {' '}
       <path
         d='M3.333 15H5v1.667h10V3.333H5V5H3.333V2.5a.833.833 0 0 1 .834-.833h11.666a.833.833 0 0 1 .834.833v15a.833.833 0 0 1-.834.833H4.167a.833.833 0 0 1-.834-.833V15ZM5 9.167h5.833v1.666H5v2.5L.833 10 5 6.667v2.5Z'
         fill={color}
-      />{' '}
-    </g>{' '}
+      />
+    </g>
     <defs>
-      {' '}
       <clipPath id='a'>
-        {' '}
-        <path fill={color} d='M0 0h20v20H0z' />{' '}
-      </clipPath>{' '}
-    </defs>{' '}
+        <path fill={color} d='M0 0h20v20H0z' />
+      </clipPath>
+    </defs>
   </svg>
 );
